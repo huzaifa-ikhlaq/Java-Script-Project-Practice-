4ee467fc94bd4d863a4a71ae38901c27112ef4ef
huzaifa ikhlaq~ <mhu300042@gmail.com>
2025-06-18 04:28:05 -0700

    complete with dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js (ES module style)
+export default {
+  darkMode: 'class',
+  content: [
+    './*.html',
+    './**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
